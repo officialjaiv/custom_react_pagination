4c7c4bdaf0b877b2e2c81bdb2db68c99a9e41e0f
officialjaiv <bhanushalijaiv@gmail.com>
2022-04-14 11:50:15 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Pagination from './Pagination';
+import './style.scss';
 
-function App() {
+let PageSize = 10;
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+  const [totalData, setTotalData] = useState();
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await fetch(`https://api.instantwebtools.net/v1/passenger?page=${currentPage}&size=${PageSize}`);
+      const data = await res.json();
+      setTotalData(data.totalPassengers)
+      setPosts(data.data);
+    };
+    getPosts();
+  }, [currentPage])
+
+  const pages = [];
+  for (let i = 0; i <= Math.ceil(totalData / PageSize); i++) {
+    pages.push(i + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>TRIPS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map(item => {
+            return (
+              <tr key={item._id}>
+                <td>{item._id}</td>
+                <td>{item.name}</td>
+                <td>{item.trips}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={pages.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
+    </>
   );
 }
-
-export default App;
